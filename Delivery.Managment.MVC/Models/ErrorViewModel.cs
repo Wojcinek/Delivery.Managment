cb1ed5d2dba4206a7c0f5f4994f323b2f5650501
wojci <wojci@DESKTOP-PK2VBRC>
2023-07-10 12:09:53 +0200

    Adding MVC project

---
+++ Delivery.Managment.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Delivery.Managment.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
